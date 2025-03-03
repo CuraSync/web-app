ee40f60d4a10feb45a80cda52e0c43c7acd9595d
Dilina Perera <137534158+DilinaCP@users.noreply.github.com>
2025-03-03 12:07:53 +0530

    enhance pharmacy messaging and sidebar components
    -corrected the import path for the PharmacySidebar component in the MessagesPage component
    -updated the Sidebar component to include navigation links for Messaging, Notification, and Settings.

--- src/app/dashboard/pharmacy/sidebar/sidebar.tsx
+++ src/app/dashboard/pharmacy/sidebar/sidebar.tsx
@@ -6,10 +6,6 @@ const Sidebar = () => {
     <div>
       <aside className="w-64 border-r p-6 flex flex-col h-full">
         <nav className="mt-12 space-y-6 flex-grow">
-          <Link href="/dashboard/pharmacy/pharmacylist" className="flex items-center space-x-3 text-gray-600 hover:text-blue-600">
-            <FaPrescriptionBottleAlt className="w-5 h-5" />
-            <span>Pharmacy</span>
-          </Link>
           <Link href="/dashboard/pharmacy/message" className="flex items-center space-x-3 text-gray-600 hover:text-blue-600">
             <FaEnvelope className="w-5 h-5" />
             <span>Messaging</span>
@@ -26,8 +22,8 @@ const Sidebar = () => {
         {/* Sidebar profile */}
         <div className="mt-auto flex items-center space-x-3">
           <div className="w-10 h-10 bg-gray-200 rounded-full"></div>
-          <Link href="/dashboard/patient" className="text-gray-700 hover:text-blue-600">
-            Sarah Johnson
+          <Link href="/dashboard/pharmacy" className="text-gray-700 hover:text-blue-600">
+            Pharmacy Profile
           </Link>
         </div>
       </aside>
